7d825d9eeb0747083bd61e1c471a495767defdd1
Scott Murray <rai.five@gmail.com>
2017-10-04 16:41:21 +0100

    Artist Class Initialized

--- models/artist.rb
+++ models/artist.rb
@@ -1,1 +1,27 @@
-require 
+require 'pg'
+require_relative '../db/sql_runner'
+
+
+class Artist
+
+attr_accessor :artist_name
+attr_reader :id
+
+def initialize(options)
+  @artist_name =
+  @id = options['id'].to_i if options['id']
+
+end
+
+def save()
+  db = PG.connect({dbname: 'music_collection', host: 'localhost'})
+  sql = "INSERT INTO artists (artist_name) VALUES ($1)"
+
+  values = [@artist_name]
+  db.prepare("save_customer", sql)
+  db.exec_prepared("save_customer", values)
+  db.close
+end
+
+
+end
